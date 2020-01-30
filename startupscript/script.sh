5c047d2601cbcdc2fe04f70217906ce6ccf4a440
Heloise Sanmiguel Niebles Noriega <heloise.sanmiguel@myclouddoor.com>
2020-01-30 14:01:40 +0100

    adding statements to script

--- startupscript/script.sh
+++ startupscript/script.sh
@@ -1,2 +1,4 @@
 SUSEConnect -r 632B0F54D50200 -e heloise.sanmiguel@myclouddoor.com
+SUSEConnect --status
+zypper --quiet patch -y --updatestack-only
 zypper --non-interactive --quiet patch -y --category security
