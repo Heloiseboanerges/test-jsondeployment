d0e57506b0c6f8317b326345bf201af956d2b989
Heloise Sanmiguel Niebles Noriega <heloise.sanmiguel@myclouddoor.com>
2020-06-03 11:35:23 +0200

    changing SLES registration code

--- startupscript/script.sh
+++ startupscript/script.sh
@@ -1,4 +1,4 @@
-SUSEConnect -r 632B0F54D50200 -e heloise.sanmiguel@myclouddoor.com
+SUSEConnect -r 172906EDC081D889 -e heloiseboanerges@hotmail.com
 SUSEConnect --status
 zypper --quiet patch -y --updatestack-only
 zypper --non-interactive --quiet patch -y --category security
