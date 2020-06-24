35c78e8c9e3db5b77e022f32ac69335be9709209
Heloise Sanmiguel Niebles Noriega <heloise.sanmiguel@myclouddoor.com>
2020-06-24 18:20:17 +0200

    added dependency on LB and logs output in startup script

--- startupscript/script.sh
+++ startupscript/script.sh
@@ -1,4 +1,11 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>log.out 2>&1
+# Everything below will go to the file 'log.out':
 SUSEConnect -r EA18E064F5CBC3A3 -e heloiseboanerges@hotmail.com
 SUSEConnect --status
 zypper --quiet patch -y --updatestack-only
 zypper --non-interactive --quiet patch -y --category security
+zypper install -y socat
+zypper install -y resource-agents
