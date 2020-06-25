8f07c7af30ad1c70c8205cc72a00cb8b10972c62
Heloise Sanmiguel Niebles Noriega <heloise.sanmiguel@myclouddoor.com>
2020-06-25 15:08:35 +0200

    cambios en script inicio, secuencialmente

--- config-script/script-config.sh
+++
@@ -1,9 +0,0 @@
-zypper install -y socat
-zypper install -y resource-agents
-tasks=$(cat /etc/systemd/system.conf | grep DefaultTasksMax=)
-sudo sed -i "s/$tasks/DefaultTasksMax=4096/g" /etc/systemd/system.conf
-systemctl daemon-reload
-systemctl --no-pager show | grep DefaultTasksMax
-
-dirty=$(cat /etc/sysconfig/network/ifcfg-eth0 | grep CLOUD_NETCONFIG_MANAGE=)
-sudo sed -i "s/$dirty/CLOUD_NETCONFIG_MANAGE="no"/g" /etc/sysconfig/network/ifcfg-eth0
