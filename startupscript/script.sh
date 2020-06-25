307b35ff3bb7a7b400f7fb00bd7d702002ce6c66
Heloise Sanmiguel Niebles Noriega <heloise.sanmiguel@myclouddoor.com>
2020-06-25 12:30:09 +0200

    cambios en script inicio, secuencialmente

--- startupscript/script.sh
+++ startupscript/script.sh
@@ -2,3 +2,5 @@ SUSEConnect -r EA18E064F5CBC3A3 -e heloiseboanerges@hotmail.com
 SUSEConnect --status
 zypper --quiet patch -y --updatestack-only
 zypper --non-interactive --quiet patch -y --category security
+
+sh /var/lib/waagent/custom-script/download/0/script-config.sh
